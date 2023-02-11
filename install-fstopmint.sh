9bdf0ea3932384293db968aa0024c4dea91f2715
NodeMixaholic <sam@sllord.info>
2023-02-11 05:41:57 -0600

    Update install-fstopmint.sh

--- install-fstopmint.sh
+++ install-fstopmint.sh
@@ -1,13 +1,20 @@
-sudo apt update
 sudo dpkg --add-architecture i386
 sudo apt update
-sudo apt install -y git wget wine dosbox flatpak snapd python3 python3-pip
-
+sudo apt install -y git wget wine dosbox flatpak snapd python3 python3-pip firmware-linux-nonfree firmware-misc-nonfree
 sudo wget -O /usr/bin/node-get https://raw.githubusercontent.com/F-Stop-Technologies/node-get/main/node-get
 sudo chmod +x /usr/bin/node-get
 
-sudo wget -O /tmp/sid-do-sir.sh https://raw.githubusercontent.com/F-Stop-Technologies/lmde-to-sid/main/lmde2sid.sh
-sudo bash /tmp/sid-do-sir.sh
+#!/bin/bash
+
+# Check if NVIDIA card is present
+echo "Check for NVIDIA card..."
+if lspci | grep -i NVIDIA; then
+  echo "NVIDIA card detected. Installing closed-source NVIDIA drivers..."
+  sudo apt install -y nvidia-driver
+else
+  echo "No NVIDIA card detected. Not installing NVIDIA drivers..."
+fi
+
 
 sudo mkdir /fstop-python-nodefun/
 sudo git clone https://github.com/F-Stop-Technologies/yolov8-fstop /fstop-python-folderimagedetection/
@@ -29,5 +36,6 @@ rm $ffpath/prefs.js
 sudo -u $USER wget -O $ffpath/prefs.js https://raw.githubusercontent.com/F-Stop-Technologies/prefs.js/main/prefs.js
 
 pip3 install ultralytics || echo "Error installing ultralytics, you won't be able to use image detection!"
+sudo apt upgrade #Note: avoid dist-upgrade!
 echo "Installed! Rebooting in 30 seconds.... (CTRL+Z to abort)"
 bash -c "sleep 30; shutdown -r now"
