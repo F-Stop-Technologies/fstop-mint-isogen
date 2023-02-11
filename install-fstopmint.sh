4ec3c6ceeb329f40e64e0b70bf04a91d26da37aa
NodeMixaholic <sam@sllord.info>
2023-02-10 20:49:10 -0600

    Update install-fstopmint.sh

--- install-fstopmint.sh
+++ install-fstopmint.sh
@@ -1,7 +1,7 @@
 sudo apt update
 sudo dpkg --add-architecture i386
 sudo apt update
-sudo apt install git wget wine dosbox flatpak
+sudo apt install git wget wine dosbox flatpak python3 python3-pip
 sudo wget -O /usr/bin/node-get https://raw.githubusercontent.com/F-Stop-Technologies/node-get/main/node-get
 sudo chmod +x /usr/bin/node-get
 sudo wget -O /tmp/sid-do-sir.sh https://raw.githubusercontent.com/F-Stop-Technologies/lmde-to-sid/main/lmde2sid.sh
