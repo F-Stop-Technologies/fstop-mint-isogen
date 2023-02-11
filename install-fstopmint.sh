505a0a8e166321d1168732029ed1b4123c1e52eb
NodeMixaholic <sam@sllord.info>
2023-02-11 06:34:58 -0600

    Update install-fstopmint.sh

--- install-fstopmint.sh
+++ install-fstopmint.sh
@@ -15,7 +15,7 @@ else
   echo "No NVIDIA card detected. Not installing NVIDIA drivers..."
 fi
 
-echo "sudo node-get install net.brinkervii.grapejuice" > /usr/bin/roblox-easyinstaller
+echo "sudo node-get install net.brinkervii.grapejuice" > /usr/bin/roblox-easyinstall
 
 sudo mkdir /fstop-python-nodefun/
 sudo wget -O /fstop-python-nodefun/duckbart.py https://raw.githubusercontent.com/F-Stop-Technologies/duckbart-fstop/main/main.py
