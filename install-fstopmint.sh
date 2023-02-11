513e6944ac1fbdd5170a64c6ad885f54cc2927f5
NodeMixaholic <sam@sllord.info>
2023-02-11 07:32:43 -0600

    Update install-fstopmint.sh

--- install-fstopmint.sh
+++ install-fstopmint.sh
@@ -2,7 +2,7 @@
 
 sudo dpkg --add-architecture i386
 sudo apt update
-sudo apt install genisoimage unzip debian-installer
+sudo apt install genisoimage unzip 7zip debian-installer
 sudo apt install -y git wget wine dosbox flatpak snapd python3 psmisc python3-pip bcmwl-kernel-source firmware-linux-nonfree firmware-misc-nonfree
 sudo wget -O /usr/bin/node-get https://raw.githubusercontent.com/F-Stop-Technologies/node-get/main/node-get
 sudo chmod +x /usr/bin/node-get
