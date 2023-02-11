518936e48ea93eff77c4fbbb98bc34513182d8cc
NodeMixaholic <sam@sllord.info>
2023-02-11 05:59:43 -0600

    Update install-fstopmint.sh

--- install-fstopmint.sh
+++ install-fstopmint.sh
@@ -37,6 +37,5 @@ rm $ffpath/prefs.js
 sudo -u $USER wget -O $ffpath/prefs.js https://raw.githubusercontent.com/F-Stop-Technologies/prefs.js/main/prefs.js
 
 pip3 install ultralytics || echo "Error installing ultralytics, you won't be able to use image detection!"
-NEEDRESTART_MODE=a apt dist-upgrade #Note: only run dist-upgrade once a week!
 echo "Installed! Rebooting in 30 seconds.... (CTRL+Z to abort)"
 bash -c "sleep 30; shutdown -r now"
