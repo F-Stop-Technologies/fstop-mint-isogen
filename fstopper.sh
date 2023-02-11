68be1ee350f783df5906dc98e3ef0b0b1d9c6c96
NodeMixaholic <sam@sllord.info>
2023-02-11 14:45:07 -0600

    Update fstopper.sh

--- fstopper.sh
+++ fstopper.sh
@@ -23,33 +23,10 @@ sudo chmod +x /usr/bin/nvidia-easyinstall
 echo "sudo node-get install net.brinkervii.grapejuice" > /usr/bin/roblox-easyinstall
 chmod +x /usr/bin/roblox-easyinstall
 
-sudo rm -rf /fstop-wallpapers/  || echo "Wallpapers not installed yet, not reinstalling but installing."
-sudo git clone https://github.com/F-Stop-Technologies/wallpapers /fstop-wallpapers/
-sudo chmod -R 777 /fstop-wallpapers
-
-sudo rm -rf /fstop-python-nodefun/ || echo "NodeFun not installed yet, not reinstalling but installing."
-sudo mkdir /fstop-python-nodefun/
-sudo wget -O /fstop-python-nodefun/duckbart.py https://raw.githubusercontent.com/F-Stop-Technologies/duckbart-fstop/main/main.py
-sudo wget -O /fstop-python-nodefun/blog-gen.py https://raw.githubusercontent.com/F-Stop-Technologies/blogger_openai_fstop/main/main.py
-
-sudo rm -rf /fstop-python-folderimagedetection/ || echo "Image detection not installed yet, not reinstalling but installing."
-sudo git clone https://github.com/F-Stop-Technologies/yolov8-fstop /fstop-python-folderimagedetection/
-sudo wget -O /fstop-python-folderimagedetection/recognize.py https://raw.githubusercontent.com/F-Stop-Technologies/yolov8-imageobjectdetection/main/detect.py
-sudo wget -O /fstop-python-folderimagedetection/yolov8n.pt https://github.com/ultralytics/assets/releases/download/v0.0.0/yolov8n.pt
-
-sudo apt install -y python3-torch python3-tk python3-numpy python3-torchvision
-
-sudo -u $USER pip3 install openai duckduckgo3 requests python-dotenv colorama --break-system-packages
-sudo -u $USER pip3 install opencv-python --break-system-packages
-sudo -u $USER pip3 install ultralytics --break-system-packages || echo "Error installing ultralytics, you won't be able to use image detection!"
-
-sudo rm -rf /fstop-spacy-nlt/ || echo "NLP not installed yet, not reinstalling but installing."
-sudo mkdir /fstop-spacy-nlp/
-sudo wget -O /fstop-spacy-nlp/spacy-nlp.py https://raw.githubusercontent.com/F-Stop-Technologies/spacy-nlp/main/spacy-nlp.py
-sudo -u $USER pip3 install spacy --break-system-packages || echo "Error installing spacy, you will not be able to use nlp!"
-python -m spacy download en_core_web_sm || echo "Error installing spacy en_core_web_sm, you will not be able to use nlp!"
-python -m spacy download en || echo "Error installing spacy en, you will not be able to use nlp!"
-
+sudo rm /tmp/update-custom || "Update custom not exist (normal)"
+sudo wget -O /tmp/update-custom https://raw.githubusercontent.com/F-Stop-Technologies/node-get/main/update-custom-goodies
+sudo bash /tmp/update-custom
+  
 rm -rf /home/tester/.mozilla
 
 rm -rf /linux-live
